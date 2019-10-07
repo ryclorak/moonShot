e4dd27cfdcbdec446c7ae92fac8da1c2feef86bd
ryclorak <ryclorak@gmail.com>
2019-10-06 22:03:56 -0700

    scripts for moon and rocket motion

---
+++ moon.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*public class moon : MonoBehaviour
+{
+	
+	public Vector2 velocity = new Vector2(1,0);
+	
+	[Range(0,5)]
+	public float rotateSpeed = 1f;
+	[Range(0,5)]
+	public float rotateRadiusX = 1f;
+	[Range(0,5)]
+	public float rotateRadiusY = 1f;
+	
+	public bool Clockwise = true;
+	
+	private Vector2 _centre;
+	private float _angle;
+	
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _centre = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _centre += velocity * Time.deltaTime;
+		
+		_angle += (Clockwise ? rotateSpeed : -rotateSpeed) * Time.deltaTime;
+		
+		var x = Mathf.Sin(_angle) * rotateRadiusX;
+		var y = Mathf.Cos(_angle) * rotateRadiusY;
+		
+		transform.position = _centre + new Vector2(x, y);
+    }
+}
+*/
+
+
+
+public class moon : MonoBehaviour
+{
+	
+	private float rotateSpeed = .5f;
+	private float radius = 2f;
+	
+	private Vector2 _centre;
+	private float _angle;
+	
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _centre = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _angle += rotateSpeed * Time.deltaTime;
+		
+		var offset = new Vector2(Mathf.Sin(_angle), Mathf.Cos(_angle)) * radius;
+		//var offset = new Vector2(_angle, _angle);
+		transform.position = _centre + offset;
+    }
+}
---
+++ rocket2.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocket2 : MonoBehaviour
+{
+	private Rigidbody2D rb;
+	private float moveInput;
+	private bool isGrounded;
+	
+	public float speed;
+	public float jumpForce;
+	public float checkRadius;
+	public Transform groundCheck;
+	public LayerMask whatIsGround;
+	
+	
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+	
+	void FixedUpdate()
+	{
+		isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+		
+		moveInput = Input.GetAxis("Vertical");
+		rb.velocity = new Vector2(rb.velocity.x, moveInput * speed);
+	}
+	
+	
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+		{
+			rb.velocity = Vector2.up * jumpForce;
+		}
+    }
+}
